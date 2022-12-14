8cac19a98fd2232fd945766127723d2daae8d5e3
Brittnay <bphilbrook1@gmail.com>
2022-12-14 07:07:13 -0500

    Added footer with GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Britt RUmery and is
+        <a
+          href="https://github.com/brittrumery/weatherproject"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
