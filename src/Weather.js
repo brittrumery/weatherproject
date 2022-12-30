2876cca901da2d55a9f3c37041d0f3ba66dbf00e
Brittnay <bphilbrook1@gmail.com>
2022-12-30 13:51:12 -0500

    Added SheCodes API and started integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,52 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      feels: response.data.temperature.feels_like,
+      description: response.data.condition.description,
+    });
+
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Wednesday 6:47</li>
+          <li>{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
             />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
 
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 6:47</li>
-        <li>Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />
-
-          <div className="tempElement">
-            30<span className="unit">°F</span>
+            <div className="tempElement">
+              {Math.round(weatherData.temperature)}
+              <span className="unit">°F</span>
+            </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 3%</li>
-            <li>Humidity: 87%</li>
-            <li>Wind: 15 mph</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>
+                Feels Like: {weatherData.feels}
+                °F
+              </li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} mph</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "41a260f121coebc3t902bb88f6dc8373";
+    let city = "New York";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiURL).then(handleResponse);
+
+    return "Loading...";
+  }
 }
